9b5d346e2c7d0975ec6662a41f47acd2f8117773
Nikita Rossik <born2lose@protonmail.com>
2022-12-03 16:47:13 +0300

    Day 2.

--- Sources/AdventOfCode/AdventOfCode.swift
+++ Sources/AdventOfCode/AdventOfCode.swift
@@ -1,9 +1,74 @@
 import Foundation
 
+/*
+ Opponent:
+ A — Rock: 1
+ B — Paper: 2
+ C — Scissors: 3
+
+ You:
+ X — Rock: 1
+ Y — Paper: 2
+ Z — Scissors: 3
+
+ ##############
+ ##############
+
+ Lost 	— 0
+ Draw 	— 3
+ Won 	— 6
+ */
+
+// enum MoveParseCharacter: Error {
+// 	case emptyCharacter(String)
+// 	case matchingCharacter(String)
+// }
+
+// enum CurrentMove: Int {
+// 	case rock = 1
+// 	case paper = 2
+// 	case scissors = 3
+
+// 	init(_ c: Character) throws {
+// 		guard !c.unicodeScalars.isEmpty else {
+// 			throw MoveParseCharacter.emptyCharacter("Character can't be empty.")
+// 		}
+
+// 		switch c.uppercased() {
+// 		case "A", "X": self = .rock
+// 		case "B", "Y": self = .paper
+// 		case "C", "Z": self = .scissors
+// 		default:
+// 			throw MoveParseCharacter.matchingCharacter("Can't match the \(c.uppercased()) move.")
+// 		}
+// 	}
+// }
+
+// enum MovePoint: Int {
+// 	case lost = 0
+// 	case draw = 3
+// 	case won = 6
+// }
+
+// 		let input = """
+// 		A Y
+// 		B X
+// 		C Z
+// 		"""
+
+// 		let x = input
+// 			.split(separator: "\n")
+// 			.map {
+// 				let opponent = try? CurrentMove($0.first!)
+// 				let you = try? CurrentMove($0.dropFirst(2).first!) // Y, X, Z
+// 				// Move Point result
+// 			}
+
+// 		print(x)
+
 @main
 public enum AdventOfCode {
 	public static func main() {
-		let result = dayOne()
-		print(result.0!)
+		dayTwo()
 	}
 }
---
+++ Sources/AdventOfCode/day_02.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public func dayTwo() {
+	var moves: [String: [Int]] {
+		[
+			"A X": [1 + 3, 3 + 0],
+			"A Y": [2 + 6, 1 + 3],
+			"A Z": [3 + 0, 2 + 6],
+			"B X": [1 + 0, 1 + 0],
+			"B Y": [2 + 3, 2 + 3],
+			"B Z": [3 + 6, 3 + 6],
+			"C X": [1 + 6, 2 + 0],
+			"C Y": [2 + 0, 3 + 3],
+			"C Z": [3 + 3, 1 + 6],
+		]
+	}
+
+	var totals = (0, 0)
+
+	print("CMD + V. Then CTRL + D to end of file:\n")
+
+	while let input = readLine() {
+		totals.0 += moves[input]!.first!
+		totals.1 += moves[input]![1]
+	}
+
+	print("Puzzle 1: \(totals.0)\nPuzzle 2: \(totals.1)")
+}
